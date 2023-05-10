d49efd11ef463e8b659d19c11d31b1de823d59a3
Nikolaos Giagkinis <giagkinis.nick@gmail.com>
2023-05-10 12:59:09 +0300

    AddItem method

--- src/main/java/com/booleanuk/LearningPlan.java
+++ src/main/java/com/booleanuk/LearningPlan.java
@@ -1,4 +1,25 @@
 package com.booleanuk;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class LearningPlan {
+
+    ArrayList<String> planItems;
+    ArrayList<Integer> planCosts;
+    int capacity;
+
+    public LearningPlan() {
+        planItems = new ArrayList<>();
+        planCosts = new ArrayList<>();
+    }
+    public boolean addItem(String name, int cost) {
+        if (capacity < planItems.size()) {
+            return false;
+        } else {
+            planItems.add(name);
+            planCosts.add(cost);
+            return true;
+        }
+    }
 }
