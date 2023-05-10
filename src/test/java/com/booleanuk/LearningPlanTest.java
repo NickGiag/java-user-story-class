b17cc75c634a501a4f778c91eaa25c8e584acd4c
Nikolaos Giagkinis <giagkinis.nick@gmail.com>
2023-05-10 13:17:54 +0300

    Changes to remove item should be true to not throw outOfBounds error

--- src/test/java/com/booleanuk/LearningPlanTest.java
+++ src/test/java/com/booleanuk/LearningPlanTest.java
@@ -31,4 +31,29 @@ public class LearningPlanTest {
         Assertions.assertFalse(learningPlan.addItem(item, itemPrice));
 
     }
+
+    @Test
+    public void removeItemShouldReturnTrueAndRemovedFromBothLists() {
+        LearningPlan learningPlan = new LearningPlan();
+        learningPlan.capacity = 1;
+        String item = "Java Introduction";
+        int itemPrice = 20;
+        learningPlan.addItem(item, itemPrice);
+
+
+
+        Assertions.assertTrue(learningPlan.removeItem(item));
+        Assertions.assertEquals(0, learningPlan.planItems.size());
+        Assertions.assertEquals(0, learningPlan.planCosts.size());
+
+    }
+
+    @Test
+    public void removeItemShouldReturnFalse() {
+        LearningPlan learningPlan = new LearningPlan();
+        learningPlan.capacity = 1;
+
+
+        Assertions.assertFalse(learningPlan.removeItem("item"));
+    }
 }
