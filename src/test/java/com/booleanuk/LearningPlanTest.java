4c18b294da50eeb48483de1d360f5ce7c501edf4
Konstantinos Vrs <it21712@hua.gr>
2023-05-10 12:50:34 +0300

    LearningPlanTest, LearningPlan added

---
+++ src/test/java/com/booleanuk/LearningPlanTest.java
@@ -0,0 +1,18 @@
+package com.booleanuk;
+
+import org.junit.jupiter.api.Test;
+
+public class LearningPlanTest {
+
+    @Test
+    public void addItemReturnsTrueOnSuccessAndBothListsArePopulated(){
+
+        LearningPlan learningPlan = new LearningPlan();
+        String item = "Java Introduction";
+        int itemPrice = 20;
+
+        //learningPlan.add()
+
+
+    }
+}
